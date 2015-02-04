141243a1c9173621545484a4057525d575f8590d
Jason Yeomans <jyeo098@gmail.com>
2015-02-04 18:08:07 -0500

    gitignore

--- easypage/models.py
+++ easypage/models.py
@@ -1,22 +1,22 @@
 # coding=utf8
 from django.db import models
 from django.core.exceptions import ValidationError
-from blanc_basic_assets.models import Image
+from blanc_basic_assets.fields import AssetForeignKey
 
 class QuickLink(models.Model):
-    image_file = models.ForeignKey(Image)
+    image_file = AssetForeignKey('assets.Image')
     heading = models.CharField(max_length=20)
     body = models.CharField(max_length=250)
     button_text = models.CharField(max_length=20, default=u'View details »')
     link = models.CharField(max_length=300)
 
     def __str__(self):              # __unicode__ on Python 2
-        return self.title
+        return self.heading
     def __unicode__(self):              # __unicode__ on Python 2
         return unicode(self.heading)
 
 class Carousel(models.Model):
-    image_file = models.ForeignKey(Image)
+    image_file = AssetForeignKey('assets.Image')
     heading = models.CharField(max_length=50)
     body = models.CharField(max_length=150)
     button_text = models.CharField(max_length=20)
