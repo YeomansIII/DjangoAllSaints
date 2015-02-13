f2d0d5b99f7751f60f4efb90033cfe047ffcdea2
Jason Yeomans <jyeo098@gmail.com>
2015-02-13 02:15:48 -0500

    homepage carousel changes

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,33 +1,40 @@
 $(document).ready(function() {
 
-  var clickEvent = false;
-  $('#myCarousel').carousel({
-    interval: 4000
-  }).on('click', '.list-group li', function() {
-    clickEvent = true;
-    $('.list-group li').removeClass('active');
-    $(this).addClass('active');
-  }).on('slid.bs.carousel', function(e) {
-    if (!clickEvent) {
-      var count = $('.list-group').children().length - 1;
-      var current = $('.list-group li.active');
-      current.removeClass('active').next().addClass('active');
-      var id = parseInt(current.data('slide-to'));
-      if (count == id) {
-        $('.list-group li').first().addClass('active');
-      }
-    }
-    clickEvent = false;
-  });
+    var clickEvent = false;
+    $('#myCarousel').carousel({
+        interval: 4000
+    }).on('click', '.list-group li', function() {
+        clickEvent = true;
+        $('.list-group li').removeClass('active');
+        $(this).addClass('active');
+    }).on('slid.bs.carousel', function(e) {
+        if (!clickEvent) {
+            var count = $('.list-group').children().length - 1;
+            var current = $('.list-group li.active');
+            current.removeClass('active').next().addClass('active');
+            var id = parseInt(current.data('slide-to'));
+            if (count == id) {
+                $('.list-group li').first().addClass('active');
+            }
+        }
+        clickEvent = false;
+    });
 })
 
+function canvas_size() {
+    var offset = $('#quicklinks').offset();
+    $('#home-background').css('height', offset.top);
+}
+
 $(window).load(function() {
-  var boxheight = $('#myCarousel .carousel-inner').innerHeight();
-  var itemlength = $('#myCarousel .item').length;
-  var triggerheight = Math.round(boxheight / itemlength + 1);
-  $('#myCarousel .list-group-item').outerHeight(triggerheight);
+    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+    var itemlength = $('#myCarousel .item').length;
+    var triggerheight = Math.round(boxheight / 5);
+    $('#myCarousel .list-group-item').outerHeight(triggerheight);
 
-  var offset = $('#quicklinks').offset();
-  $('#home-background').css('height', offset.top+20);
+    canvas_size();
+});
 
+$(window).resize( function() {
+    canvas_size();
 });
