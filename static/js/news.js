354cf8ecffb48acc7b9ba0a639635bef1a472be9
Jason Yeomans <jyeo098@gmail.com>
2015-02-15 06:36:24 -0500

    fixed news animations!

--- static/js/news.js
+++ static/js/news.js
@@ -1,22 +1,23 @@
 function getArticleText(abs_url, id) {
     if ($('.read-more:eq(' + id + ')').html() === "Read More") {
-        //if ($('#news-content-' + id + '.news-text').html() === "") {
-        console.log("Ajaxing");
-        $.ajax({
-                url: "/newstext" + abs_url,
-            })
-            .done(function(data) {
-                $('#news-content-' + id + ' .news-text').html("");
-                $('#news-content-' + id + ' .news-teaser').hide();
-                $('#news-content-' + id + ' .news-text').append(data).slideDown("slow");
-                //$('#news-content-' + id + ' .news-text').show('slow');
-                $('.read-more:eq(' + id + ')').html("Read Less");
-            });
-        //} else {
-        //    $('#news-content-' + id + ' .news-teaser').show();
-        //        $('#news-content-' + id + ' .news-text').hide();
-        //        $('.read-more:eq(' + id + ')').html("Read More");
-        //    }
+        if ($('#news-content-' + id + ' .news-text').html() === "") {
+            console.log("Ajaxing");
+            $.ajax({
+                    url: "/newstext" + abs_url,
+                })
+                .done(function(data) {
+                    $('#news-content-' + id + ' .news-text').html("");
+                    $('#news-content-' + id + ' .news-teaser').hide('fast');
+                    $('#news-content-' + id + ' .news-text').hide();
+                    $('#news-content-' + id + ' .news-text').append(data).slideDown('slow');
+                    //$('#news-content-' + id + ' .news-text').show('slow');
+                    $('.read-more:eq(' + id + ')').html("Read Less");
+                });
+        } else {
+            $('#news-content-' + id + ' .news-teaser').hide('fast');
+            $('#news-content-' + id + ' .news-text').slideDown('slow');
+            $('.read-more:eq(' + id + ')').html("Read Less");
+        }
     } else {
         $('#news-content-' + id + ' .news-text').slideUp('slow', function() {
             $('#news-content-' + id + ' .news-teaser').show('fast');
