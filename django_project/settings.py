ee8ca1441fad4a2ba57f6b500e2e0725ca2054e7
Jason Yeomans <jyeo098@gmail.com>
2019-05-16 01:46:37 -0400

    fix errors

--- django_project/settings.py
+++ django_project/settings.py
@@ -56,32 +56,7 @@ ROOT_URLCONF = 'django_project.urls'
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
-import ldap
-from django_auth_ldap.config import LDAPSearch, PosixGroupType
-
-AUTH_LDAP_SERVER_URI = "ldap://yeomans.io"
-
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=groups,dc=yeomans,dc=io",
-    ldap.SCOPE_SUBTREE, "(objectClass=posixGroup)"
-)
-AUTH_LDAP_GROUP_TYPE = PosixGroupType()
-AUTH_LDAP_CACHE_GROUPS = True
-
-####Replace with proper group name for the website you are working on.  Group should be created in yeomans.io ldap admin, contact jason@yeomans.us to add a group
-AUTH_LDAP_REQUIRE_GROUP = "cn=allsaintsdjango,ou=groups,dc=yeomans,dc=io"
-AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn"}
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    "is_staff": "cn=allsaintsdjango,ou=groups,dc=yeomans,dc=io",
-    "is_superuser": "cn=allsaintsdjangosuper,ou=groups,dc=yeomans,dc=io"
-}
-
-AUTH_LDAP_BIND_DN = ""
-AUTH_LDAP_BIND_PASSWORD = ""
-AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=yeomans,dc=io",
-    ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
-
 AUTHENTICATION_BACKENDS = (
-    'django_auth_ldap.backend.LDAPBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
